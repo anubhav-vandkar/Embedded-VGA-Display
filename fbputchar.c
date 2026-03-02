31e85358276f61da8b11a84be25bb98213e888d9
anubhav-vandkar <av3336@columbia.edu>
2026-03-02 15:50:13 -0500

    backspace fix

--- fbputchar.c
+++ fbputchar.c
@@ -143,10 +143,11 @@ void screen_shift(char screen_buffer[20][64], char *new_content){
     }
     fbputs((const char *)screen_buffer[i], i+1, 0);
   }
+
 }
 
 void fbcursor(int row, int col, char *input_buffer){
-  for(int i = 0; i < strlen(input_buffer)-1; i++){
+  for(int i = 0; i < strlen(input_buffer); i++){
     if(i == (row-22) * 64 + col)
       fbputchar(input_buffer[i], row, col);
     else
