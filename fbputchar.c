4f261386cb3208cce71681718357b2d494067fd3
anubhav-vandkar <av3336@columbia.edu>
2026-03-04 15:51:21 -0500

    fix

--- fbputchar.c
+++ fbputchar.c
@@ -161,14 +161,13 @@ void fbcursor(int pos, char *input_buffer){
 
 void shift_text_left(int pos, char * input_buffer){
   int ends = strlen(input_buffer);
+  fbputchar(' ', 22 + ends / 64, ends % 64);
   input_buffer[ends-1] = '\0';
   memset(input_buffer + ends - 1, 0, 1);
-  fbputchar(' ', 22 + ends / 64, ends % 64);
-  for(int i = pos; i < ends; i++){
+  for(int i = pos+1; i < ends; i++){
     input_buffer[i] = input_buffer[i+1];
     fbputchar(input_buffer[i], 22 + i / 64, i % 64);
   }
-
 }
 
 static unsigned char font[] = {
